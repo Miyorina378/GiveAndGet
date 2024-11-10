2373ff496af087e8d17463028a22a901d78f3997
Miyorina378 <darendianana@gmail.com>
2024-11-10 18:27:25 +0700

    Add Register System

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,5 +1,10 @@
+# users_app/urls.py
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
+from . import views
 
 urlpatterns = [
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', view=views.register, name='register'),
     path('', include("django.contrib.auth.urls")),
 ]
