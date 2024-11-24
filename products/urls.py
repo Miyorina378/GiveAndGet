a5a39c6723293e8d8993a617fef6b2216566edc3
Chatchai Seekram <6610685064@cn102>
2024-11-24 18:37:30 +0700

    edit products

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),  # URL สำหรับรายละเอียดสินค้า
 ]
