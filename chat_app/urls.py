b38f3e41189a50c2b7a498243b9f8e3972b403a2
Miyorina378 <darendianana@gmail.com>
2024-12-17 10:09:26 +0700

    Fix TradeRequest

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('<str:room_name>/', views.chat_room, name='chat_room'),
     path('get-user-products/<str:room_name>/', views.get_user_products, name='get_user_products'),
+    path('send_trade_request/', views.send_trade_request, name='send_trade_request'),
+
 ]
 
 
--- products/views.py
+++ products/views.py
@@ -81,14 +81,6 @@ def edit_product(request, product_id):
 
     return render(request, 'products/add_product.html', {'form': form, 'is_edit': True})
 
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
-from users_app.models import Report
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
 @login_required
 def add_report(request):
     if request.method == 'POST':
